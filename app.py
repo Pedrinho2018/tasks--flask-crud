794ad9b666ec2fdbc4b6bb511d74b490fef9429f
Pedro Henrique Freitas Silva <pedrohenriksk8@gmail.com>
2026-01-13 22:08:03 -0300

    update

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models import Task
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Flask!"
-
-@app.route('/about')   
-def about():
-    return "Sobre a aplicação Flask."
+tasks = []
+task_id_control = 1
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control, title=data['title'], description=data['description'])
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({'message': 'tarefa criada com sucesso'}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from .task import task as Task
+
+__all__ = ['Task']
